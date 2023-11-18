d52964ac8eafcd6d7c6e3e20c7d2165682b02318
tsthuy <huydo282003@gmail.com>
2023-11-18 11:16:29 +0700

    Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import About from "./pages/About"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/Signin"
 import SignUp from "./pages/Signup"
+import Header from "./components/Header"
 export default function App() {
   return ( 
     <BrowserRouter>
+    <Header />
     <Routes>
+      
       <Route path="/" element={<Home/>}  />
       <Route path="/about" element={<About/>}  />
       <Route path="/profile" element={<Profile/>}  />
