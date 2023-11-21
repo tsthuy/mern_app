c36839dd87753c7b6e8b9f613cb82f4dad698279
tsthuy <huydo282003@gmail.com>
2023-11-21 15:51:40 +0700

    complete signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/model.user.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../ultis/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   console.log("body", req.body);
@@ -11,6 +13,23 @@ export const signUp = async (req, res, next) => {
     res.status(200).json("created new user completed!!!");
   } catch (error) {
     // res.status(500).json(error.message);
+    next(error);
+  }
+};
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!!!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credential!!!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
